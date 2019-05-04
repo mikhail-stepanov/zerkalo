959d758179129e847fe8e02b56ddd7a7519ecc7f
Mikhail Stepanov <heirloon@mail.ru>
2019-05-04 20:26:48 +0300

    Initial commit

---
+++ ms-common-database/src/main/java/com/tochka/database/CommonDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.tochka.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonDatabaseApplication.class, args);
+    }
+
+}
