8def18b7118829366942c3a2aaa6aca180225ae3
Mikhail Stepanov <heirloon@mail.ru>
2019-05-10 17:58:08 +0300

    Created ms-auth

---
+++ ms-common-database/src/main/java/com/tochka/database/enums/AdminRightsEnum.java
@@ -0,0 +1,16 @@
+package com.tochka.database.enums;
+
+public class AdminRightsEnum {
+
+    public static final String RIGHT_ACCOUNT = "account";
+
+    public static final String RIGHT_ACCOUNT_UNIT = "account-unit";
+
+    public static final String RIGHT_ACCOUNT_USER = "account-user";
+
+    public static final String RIGHT_IDENTIFIER_GLN = "identifier-gln";
+
+    public static final String RIGHT_ADMIN_USER = "admin-user";
+
+    public static final String RIGHT_CONTRACTOR = "contractor";
+}
